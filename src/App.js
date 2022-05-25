b5b2d893c14b73eef2ad309696d1d22b034ece88
cesar david moreno <cesardaviddev@gmail.com>
2022-05-25 23:02:08 +0200

    REFACTORING: Sections todo and done make

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Header } from './components/Header.js'
 import { Form } from './components/Form.js';
-import {Footer } from './components/Footer.js'
+import { Footer } from './components/Footer.js'
+import { Sections } from './components/Sections.js'
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <section className="mainCointainer">
-           <Header />
+      <section className="mainCointainer">
+        <Header />
            <Form />
-           <Footer />
-         </section>
+           <Sections title='Todo' />
+           <Sections title='Done' />
+      </section>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
