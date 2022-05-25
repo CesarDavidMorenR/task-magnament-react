302c8387992a839c2e353d99efaa9fcc866c3e95
cesar david moreno <cesardaviddev@gmail.com>
2022-05-25 22:55:18 +0200

    REFACTORING: Make footer a component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header.js'
 import { Form } from './components/Form.js';
+import {Footer } from './components/Footer.js'
 // import './App.css';
 
 function App() {
@@ -9,89 +10,8 @@ function App() {
         <section className="mainCointainer">
            <Header />
            <Form />
-            <table>
-                  <thead>
-                     <tr>
-                        <td colspan="3" class="borde">TO-DO</td>
-                     </tr>
-                  </thead>
-                <tbody>
-                     <tr>
-                     <tr>
-                        <td>Creacion del header</td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     </tr>
-                     <tr>
-                     <tr>
-                        <td>Call-To-Action para Home</td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     </tr>
-                     <tr>
-                     <tr>
-                        <td>Creaación de las tablas en la BD</td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     </tr>
-                    </tbody>
-            </table>
+           <Footer />
          </section>
-        
-        <section>
-               <br />
-               <table>
-                  <thead>
-                     <tr>
-                        <td colspan="3" class="borde">DONE</td>
-                     </tr>
-                  </thead>
-              <tbody>
-                     <tr>
-                     <tr>
-                        <td>Componente Card</td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     </tr>
-                     <tr>
-                     <tr>
-                        <td>Imágemes profile</td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                     <tr>
-                        <td></td>
-                     </tr>
-                </tr>
-                </tbody>
-               </table>
-         </section>
-         
-        <footer class="mainFooter">
-        <p>All rights reserved (Develop on 2022). Make by David Moreno with HTML, CSS, and JavaScript</p>
-         {/* <!-- Hecho el Jueves 10/02/2022--> */}
-         </footer>
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer class="mainFooter">
+      <p>
+        All rights reserved (Develop on 2022). Make by David Moreno with HTML,
+        CSS, and JavaScript
+      </p>
+      {/* <!-- Hecho el Jueves 10/02/2022--> */}
+    </footer>
+  );
+}
